748b5c617ed2b0c2303b647faf017d6152063747
Bill Hamilton <bill.hamilton@autodesk.com>
2014-10-15 13:41:09 -0400

    fixed

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Grid.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Grid.java
@@ -5,7 +5,6 @@ import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL;
 
 public class Grid {
 
-break this file too
 
     private static final int DEFAULT_ROW_COUNT = 3;
     private static final int DEFAULT_COLUMN_COUNT = 3;
